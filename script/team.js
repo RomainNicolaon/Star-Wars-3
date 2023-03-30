badf33b14c2d6581686721a9e67baa7fa6026ebe
AthenaisBe <a.bellereau@gmail.com>
2023-03-30 16:44:06 +0200

    jhfy

--- script/team.js
+++ script/team.js
@@ -1,27 +1,27 @@
 data_area = document.querySelector('.equipe-membre');
 var membres = [
     {
-        image: "/images/icon/user1.png",
-        name: "Member 1",
-        email: "jaaj@jaaj",
-        description: "Member 1 description"
+        image: "/images/icon/user6.png",
+        name: "Romain",
+        email: "romain.maitrephp@cefim.com",
+        description: "Désigné maitre PHP par la Super Team"
+    },
+    {
+        image: "/images/icon/user9.png",
+        name: "Titouan",
+        email: "titouan.expertdesign@cefim.com",
+        description: "Expert du design, mention spécial binôme PHP"
+    },
+    {
+        image: "/images/icon/user4.png",
+        name: "Athénaïs",
+        email: "athenais.passionjs@cefim.com",
+        description: "S'est découvert une affinité avec le JavaScript, n'a pas cédé aux avances du PHP"
     },
     {
         image: "/images/icon/user1.png",
-        name: "Member 1",
-        email: "jaaj@jaaj",
-        description: "Member 1 description"
-    },
-    {
-        image: "/images/icon/user1.png",
-        name: "Member 1",
-        email: "jaaj@jaaj",
-        description: "Member 1 description"
-    },
-    {
-        image: "/images/icon/user1.png",
-        name: "Member 1",
-        email: "jaaj@jaaj",
+        name: "Name",
+        email: "rejoignezlecefim@cefim.com",
         description: "Member 1 description"
     },
 
@@ -37,38 +37,76 @@ function allMembers() {
                 <div class="ficheMembre">
                     <div ><img src="${membre.image}" class="avatar" alt="avatar"/></div>
                     <div class="name">${membre.name}</div>
-                    <div class="email"></div>
-                    <div class="description"></div>
-                    <button type="submit" class="button_less"><img src="" alt"supprimer"/></button>
+                    <div class="email">${membre.email}</div>
+                    <div class="description">${membre.description}</div>
+                    <button type="submit" id="button_less" onclick="this.parentElement.remove()"><img  src="/images/trash-solid.svg" class="trashImg" alt"supprimer"/></button>
                 </div>`;
+                
     });
+   };
+   let buttonless = document.getElementById("button_less")
+   
+   function deleteMember () {
+    document.querySelector('.ficheMembre').remove();
+}
 
-};
+button_less.onclick = deleteMember
+
+let buttonAdd = document.getElementById("button_add");
+let form = document.getElementById("hidden");
 
 function afficheForm() {
-    
-}
+    if(getComputedStyle(hidden).display != "none"){
+        hidden.style.display = "none";
+      } else {
+        hidden.style.display = "flex";
+      }
+    };
+    buttonAdd.onclick = afficheForm;
+
+
+
 
 
 
 function addMember() {
+    let name = document.getElementById('name').value;
+    let email = document.getElementById('email').value;
+    let description = document.getElementById('description').value;
+    let img = document.getElementsByName('radioChoice').value;
     
-    newMember = {
-         
-        image: "/images/icon/user3.png",
-        name: "toto",
-        email: "jaaj@jaaj",
-        description: "Member 2 description"
-    };
 
+      
+    if (document.getElementById('imageProfile1').checked) {
+        img = document.getElementById('imageProfile1').value;
+       }
+       if (document.getElementById('imageProfile3').checked) {
+        img = document.getElementById('imageProfile3').value;
+       }
+       if (document.getElementById('imageProfile4').checked) {
+        img = document.getElementById('imageProfile4').value;
+       }
+       if (document.getElementById('imageProfile6').checked) {
+        img = document.getElementById('imageProfile6').value;
+       }
+       if (document.getElementById('imageProfile7').checked) {
+        img = document.getElementById('imageProfile7').value;
+       }
+       if (document.getElementById('imageProfile10').checked) {
+        img = document.getElementById('imageProfile10').value;
+       }
+
+
+    
+    
 
     data_area.innerHTML += `
     <div class="ficheMembre">
-        <div ><img src="${newMember.image}" class="avatar" alt="avatar"/></div>
-        <div class="name">${newMember.name}</div>
-        <div class="email"></div>
-        <div class="description"></div>
-        <button type="submit" class="button_less" onclick='']><img src="" alt"supprimer"/></button>
+        <div ><img src="${img}" class="avatar" alt="avatar"/></div>
+        <div class="name">${name}</div>
+        <div class="email">${email}</div>
+        <div class="description">${description}</div>
+        <button type="submit" id="button_less" onclick="this.parentElement.remove()"><img src="/images/trash-solid.svg" class="trashImg"  alt"supprimer"/></button>
     </div>`;
 
 };
