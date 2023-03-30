2e1e71e48d735c595dbd55d00e0f3b4237f130b9
MercureToucham <Toucham590@gmail.com>
2023-03-30 10:40:06 +0200

    css

--- script/main.js
+++ script/main.js
@@ -2,13 +2,14 @@ function navbar_open(){
     let close = document.getElementById("close")
     close.style.transform = "translateY(0px)";
     let open = document.getElementById("open")
-    open.style.display="none"
+    open.style.transform = "translateY(-200px)";
     console.log("Marche")
+    
 }
 function navbar_close(){
     let close = document.getElementById("close")
-    close.style.transform = "translateY(0px)";
     let open = document.getElementById("open")
-    open.style.display="none"
+    close.style.transform = "translateY(-200px)";
+    open.style.transform = "translateY(0px)";
     console.log("Marche")
 }
